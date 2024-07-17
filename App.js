f3ffadcd45ac5458fc7630e520853bcd35c74cb0
ROHINI KHILARE <rohini.khilare14@gmail.com>
2024-07-17 08:23:17 +0530

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View ,Button} from 'react-native';
 
 export default function App() {
+  const [cnt,setCnt]=useState(0)
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.fs}>{cnt}</Text>
+      <Button
+        title="Press me"
+        onPress={() => setCnt(cnt+1)}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +24,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  fs:{
+    fontSize:"100px"
+  }
 });
